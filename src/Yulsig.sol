eefafd2b039a523b810680628453e6f5ce0e002f
0xClandestine <clandestine.eth@proton.me>
2022-12-23 23:00:02 -0600

    ✨Yulsig draft

---
+++ src/Yulsig.sol
@@ -0,0 +1,220 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+/// @notice Minimal and gas effecient multi-signature wallet.
+/// @author 0xClandestine
+contract Yulsig {
+    /// -----------------------------------------------------------------------
+    /// Mutables
+    /// -----------------------------------------------------------------------
+
+    bytes32 private verificationHash;
+
+    uint256 private minimumSigners;
+
+    uint256 private nonce;
+
+    /// -----------------------------------------------------------------------
+    /// Construction
+    /// -----------------------------------------------------------------------
+
+    constructor(bytes32 _hashOfSigners, uint256 _minimumSigners) {
+        verificationHash = _hashOfSigners;
+        minimumSigners = _minimumSigners;
+    }
+
+    /// -----------------------------------------------------------------------
+    /// Multisig Logic
+    /// -----------------------------------------------------------------------
+
+    function getDigest() external {
+        assembly {
+            if calldatasize() {
+                /// -----------------------------------------------------------------------
+                /// 1) Compute EIP-712 Domain Separator
+                /// -----------------------------------------------------------------------
+
+                // keccak256(abi.encode(DOMAIN_TYPEHASH, keccak256(name), keccak256(version), chainid, address))
+
+                // First we copy each parameter of the separator into memory, starting at
+                // 0, then each additional variable is offset by 32*n bytes. Since we've got
+                // 5 words to store we start at pos 0x00 (0) and end at pos 0xa0 (32*5=160).
+
+                // 0x00                 DOMAIN_TYPEHASH
+                // 0x20                 keccak256(name)
+                // 0x40                 keccak256(version)
+                // 0x60                 block.chainid
+                // 0x80                 address(this)
+
+                // 0x00 -> DOMAIN_TYPEHASH
+                mstore(0x00, 0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f)
+                // 0x20 -> keccak256(name)
+                mstore(0x20, 0xcd4046335c6490bc800b62dfe4e32b5bbe64545e84e866aba69afbf5ce39f2df)
+                // 0x40 -> keccak256(version)
+                mstore(0x40, 0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6)
+                // 0x60 -> block.chainid
+                mstore(0x60, chainid())
+                // 0x80 -> address(this)
+                mstore(0x80, address())
+
+                // Then we hash the 5 variables, and store the output at pos 0x20 (32).
+                // We store the value at 0x20 in order to leave space for the eip712 prefix
+                // at pos 0x00 (0), which is needed for the the final eip712 hashed digest.
+
+                // 0x00                 DOMAIN_TYPEHASH
+                // 0x20                 DOMAIN_SEPARATOR() <------ Overwrote keccak256(name)
+                // 0x40                 keccak256(version)
+                // 0x60                 block.chainid
+                // 0x80                 address(this)
+
+                // 0x20 -> DOMAIN_SEPARATOR()
+                mstore(0x20, keccak256(0x00, 0xa0))
+
+                /// -----------------------------------------------------------------------
+                /// 2) Compute EIP-712 Message Digest
+                /// -----------------------------------------------------------------------
+
+                // 0x00                 DOMAIN_TYPEHASH
+                // 0x20                 DOMAIN_SEPARATOR()
+                // 0x40                 keccak256(version)
+                // 0x60                 ORDER_TYPEHASH     <------ Overwrote block.chainid
+                // 0x80                 address(this)
+
+                // 0x60 -> ORDER_TYPEHASH
+                mstore(0x60, 0xaa3a4d0cd4c47557a58609818667017c466f80079033a1aa81f16097da102d43)
+
+                // 0x00                 tx payload length PL  <------ Overwrote DOMAIN_TYPEHASH
+                // 0x20                 DOMAIN_SEPARATOR()
+                // 0x40                 keccak256(version)
+                // 0x60                 ORDER_TYPEHASH     <------ Overwrote block.chainid
+                // 0x80                 tx target          <------ Overwrote address(this)
+                // 0x94 (0x80 + 20)     tx value
+                // 0xb4 (0x94 + 32)     tx payload (start)
+
+                // 0x80 -> target
+                calldatacopy(0x80, 0x10, 0x14)
+                // 0x94 -> value
+                calldatacopy(0x94, 0x24, 0x20)
+                // 0x00 -> payload length
+                calldatacopy(0x00, 0x84, 0x20)
+                // 0xb4 -> payload
+                calldatacopy(0xb4, 0xa4, mload(0x00))
+
+                // 0x00                 tx payload length PL
+                // 0x20                 DOMAIN_SEPARATOR()
+                // 0x40                 message digest <------ Overwrote keccak256(version)
+                // 0x60                 ORDER_TYPEHASH
+                // 0x80                 tx target
+                // 0x94 (0x80 + 20)     tx value
+                // 0xb4 (0x94 + 32)     tx payload (start)
+                // ???? (0xb4 + PL)     current nonce
+
+                let nonceOffset := add(0xb4, mload(0x00))
+
+                // nonceOffset -> current nonce
+                mstore(nonceOffset, sload(nonce.slot))
+                // nonce.slot -> current nonce + 1
+                sstore(nonce.slot, add(mload(nonceOffset), 0x1))
+                // 0x40 -> hashed message digest
+                mstore(0x40, keccak256(0x60, add(0x74, mload(0x00))))
+
+                /// -----------------------------------------------------------------------
+                /// 3) Compute EIP-712 Digest
+                /// -----------------------------------------------------------------------
+
+                // 0x00 -> eip712 prefix
+                mstore(0x00, 0x1901)
+                // 0x00 -> eip712 digest
+                mstore(0x00, keccak256(0x00, 0x60))
+
+                /// -----------------------------------------------------------------------
+                /// 4) Signer verification
+                /// -----------------------------------------------------------------------
+
+                calldatacopy(0x40, 0x84, 0x20) // payload length
+
+                let payloadLength := mload(0x40)
+
+                let payloadTotalWords :=
+                    add(div(payloadLength, 0x20), iszero(iszero(mod(payloadLength, 0x20))))
+
+                let totalSignersCalldataOffset := add(0xa4, mul(payloadTotalWords, 0x20))
+
+                calldatacopy(0x60, totalSignersCalldataOffset, 0x20) // elements
+
+                let totalSigners := div(mload(0x60), 0x4)
+
+                let totalNonSigners := 0
+
+                // construct hash of signer addresses
+                for { let i } lt(i, totalSigners) { i := add(i, 1) } {
+                    let pos := add(0x200, mul(i, 32))
+
+                    // copy next 4 words (signer, v, r, s)
+                    calldatacopy(0x100, add(add(totalSignersCalldataOffset, 32), mul(i, 128)), 128)
+
+                    let signer := mload(0x100)
+                    let v := mload(0x120)
+                    let r := mload(0x140)
+                    let s := mload(0x160)
+
+                    // if signer is zero
+                    switch iszero(signer)
+                    case true {
+                        // note: yoinked from solady :P
+                        // If `s` in lower half order, such that the signature is not malleable.
+                        if iszero(
+                            gt(
+                                s,
+                                0x7fffffffffffffffffffffffffffffff5d576e7357a4501ddfe92f46681b20a0
+                            )
+                        ) {
+                            mstore(0x20, v)
+                            mstore(0x40, r)
+                            mstore(0x60, s)
+                            pop(
+                                staticcall(
+                                    gas(), // Amount of gas left for the transaction.
+                                    0x01, // Address of `ecrecover`.
+                                    0x00, // Start of input.
+                                    0x80, // Size of input.
+                                    0x40, // Start of output.
+                                    0x20 // Size of output.
+                                )
+                            )
+
+                            // `returndatasize()` will be `0x20` upon success, and `0x00` otherwise.
+                            let result := mload(sub(0x60, returndatasize()))
+
+                            mstore(pos, result)
+                        }
+                    }
+                    case false { totalNonSigners := add(totalNonSigners, 1) }
+                }
+
+                if iszero(eq(sload(verificationHash.slot), keccak256(0x200, 0x60))) { revert(0, 0) }
+
+                if gt(sload(minimumSigners.slot), sub(totalSigners, totalNonSigners)) {
+                    revert(0, 0)
+                }
+
+                /// -----------------------------------------------------------------------
+                /// 5) External Call
+                /// -----------------------------------------------------------------------
+
+                // 0x60 -> payload length
+                calldatacopy(0x60, 0x84, 0x20)
+                // 0x80 -> target
+                calldatacopy(0x80, 0x4, 0x20)
+                // 0xa0 -> value
+                calldatacopy(0xa0, 0x24, 0x20)
+                // 0xc0 -> payload
+                calldatacopy(0xc0, 0xa4, mload(0x60))
+
+                let success := call(gas(), mload(0x80), mload(0xa0), 0xc0, mload(0x60), 0x0, 0x0)
+
+                if iszero(success) { revert(0, 0) }
+            }
+        }
+    }
+}
---
+++ test/Yulsig.t.sol
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "./Counter.sol";
+import "../src/Yulsig.sol";
+
+contract YulsigTest is Test {
+    /// -----------------------------------------------------------------------
+    /// Testing Storage
+    /// -----------------------------------------------------------------------
+
+    Yulsig ms;
+    Counter target;
+
+    address addr0;
+    uint256 pk0;
+
+    address addr1;
+    uint256 pk1;
+
+    address addr2;
+    uint256 pk2;
+
+    /// -----------------------------------------------------------------------
+    /// Setup
+    /// -----------------------------------------------------------------------
+
+    function setUp() public {
+        (addr0, pk0) = makeAddrAndKey("addr0");
+        (addr1, pk1) = makeAddrAndKey("addr1");
+        (addr2, pk2) = makeAddrAndKey("addr2");
+
+        // address[] memory signers = new address[](3);
+
+        // signers[0] = addr0;
+        // signers[1] = addr1;
+        // signers[2] = addr2;
+
+        bytes32 hashOfSigners;
+
+        assembly {
+            mstore(mload(0x40), sload(addr0.slot))
+            mstore(add(mload(0x40), 0x20), sload(addr1.slot))
+            mstore(add(mload(0x40), 0x40), sload(addr2.slot))
+
+            hashOfSigners := keccak256(mload(0x40), 0x60)
+        }
+
+        emit log_string("hash of signer");
+        emit log_bytes32(hashOfSigners);
+
+        ms = new Yulsig(hashOfSigners, 2);
+        target = new Counter();
+    }
+
+    /// -----------------------------------------------------------------------
+    /// Helpers
+    /// -----------------------------------------------------------------------
+
+    function getSignatures_3_of_3(bytes32 digest) internal returns (bytes32[] memory sigs) {
+        sigs = new bytes32[](3*4);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(pk0, digest);
+        sigs[0] = 0;
+        sigs[1] = bytes32(uint256(v));
+        sigs[2] = bytes32(r);
+        sigs[3] = bytes32(s);
+
+        (v, r, s) = vm.sign(pk1, digest);
+        sigs[4] = 0;
+        sigs[5] = bytes32(uint256(v));
+        sigs[6] = bytes32(r);
+        sigs[7] = bytes32(s);
+
+        (v, r, s) = vm.sign(pk2, digest);
+        sigs[8] = 0;
+        sigs[9] = bytes32(uint256(v));
+        sigs[10] = bytes32(r);
+        sigs[11] = bytes32(s);
+    }
+
+    function getDigest(address _target, uint256 _value, bytes memory _payload, uint256 _nonce)
+        internal
+        returns (bytes32)
+    {
+        return keccak256(
+            abi.encodePacked(
+                bytes32(0x0000000000000000000000000000000000000000000000000000000000001901),
+                keccak256(
+                    abi.encode(
+                        0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f,
+                        0xcd4046335c6490bc800b62dfe4e32b5bbe64545e84e866aba69afbf5ce39f2df,
+                        0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6,
+                        block.chainid,
+                        address(ms)
+                    )
+                ),
+                keccak256(
+                    abi.encodePacked(
+                        bytes32(0xaa3a4d0cd4c47557a58609818667017c466f80079033a1aa81f16097da102d43),
+                        _target,
+                        _value,
+                        _payload,
+                        _nonce
+                    )
+                )
+            )
+        );
+    }
+
+    /// -----------------------------------------------------------------------
+    /// Tests
+    /// -----------------------------------------------------------------------
+
+    function testYulsig() public {
+        bytes memory payload = abi.encodeWithSelector(target.setNumber.selector, 420);
+
+        bytes32 digest = getDigest(address(target), 0, payload, 0);
+
+        bytes memory transaction = abi.encodeWithSelector(
+            Yulsig.getDigest.selector, target, 0, payload, getSignatures_3_of_3(digest)
+        );
+
+        (bool success, bytes memory returnData) = address(ms).call(transaction);
+
+        emit log_string("Return data: ");
+        emit log_bytes(returnData);
+
+        assertEq(target.number(), 420);
+        // assertEq(ms.nonce(), 1);
+    }
+}
